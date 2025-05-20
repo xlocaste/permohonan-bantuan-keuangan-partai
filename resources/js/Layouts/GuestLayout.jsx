dcaa15d89ad30854d5676fcfbca2ed4400f333aa
xlocaste <irfandi.di122@gmail.com>
2025-05-20 16:02:52 +0700

    style:background login

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -3,7 +3,12 @@ import { Link } from '@inertiajs/react';
 
 export default function Guest({ children }) {
     return (
-        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+        <div
+            className="flex flex-col justify-center items-center min-h-screen bg-center bg-cover selection:bg-red-500 selection:text-white"
+            style={{
+                backgroundImage: "url('/images/bg-lp.jpg')",
+            }}
+        >
             <div>
                 <Link href="/">
                     <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
